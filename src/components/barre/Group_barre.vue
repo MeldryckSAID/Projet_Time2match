1ad487fd8cc35ed7277df100e24e511abe204171
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 22:54:21 +0200

    component

---
+++ src/components/barre/Group_barre.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg width="1440" height="1207" viewBox="0 0 1440 1207" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M131 1196L-389 676" stroke="#FF8C00" stroke-width="30"/>
+<path d="M209.855 1178.5L-290.393 678.251" stroke="#FF8C00" stroke-width="30"/>
+<path d="M291 1165.64L-185.393 689.251" stroke="#FF8C00" stroke-width="30"/>
+<path d="M1548.86 96L1091.36 553.5" stroke="#FF8C00" stroke-width="30"/>
+<path d="M1722.61 18.6066L1173.5 567.713" stroke="#FF8C00" stroke-width="30"/>
+<path d="M1824.61 10.6066L1267 568.213" stroke="#FF8C00" stroke-width="30"/>
+</svg>
+
+</template>

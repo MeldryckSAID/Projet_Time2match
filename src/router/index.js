962b4abc9333c69f866c654e0ba0610d855e9f9f
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 22:30:59 +0200

    déclaration des pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AccueilView from '../views/AccueilView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AccueilView from "../views/AccueilView.vue";
+import TerrainsView from "../views/TerrainsView.vue";
+import PartiesView from "../views/PartiesView.vue";
+import SportsView from "../views/SportsView.vue";
+import PartenairesView from "../views/PartenairesView.vue";
+import AProposView from "../views/AProposView.vue";
+import MentionLegalesView from "../views/MentionLegalesView.vue";
+import PageCoView from "../views/PageCoView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Accueil', component: AccueilView },
-    // ici les autre routes
-  ]
-})
+    { path: "/", name: "AccueilView", component: AccueilView },
+    { path: "/terrains", name: "TerrainsView", component: TerrainsView },
+    { path: "/parties", name: "PartiesView", component: PartiesView },
+    { path: "/sports", name: "SportsView", component: SportsView },
+    { path: "/partenaires", name: "PartenairesView", component: PartenairesView },
+    { path: "/Apropos", name: "aProposView", component: AProposView },
+    { path: "/mentionlegales", name: "MentionsLegalesView", component: MentionLegalesView },
+    { path: "/pageco", name: "PageCoView", component: PageCoView },
+  ],
+});
 
-export default router
+export default router;

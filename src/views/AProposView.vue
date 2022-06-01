66b29b79f9cf4f2675dd41c694179d7f33b4270b
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 22:27:47 +0200

    pages a prpos

---
+++ src/views/AProposView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-Noirr grid place-content-center p-2">
+    <cardIlkay class="" />
+    <cardAxel class="mt-20" />
+    <cardMeldryck class="mt-20" />
+  </div>
+</template>
+
+<script> 
+
+import cardIlkay from "../components/card/cardIlkay.vue";
+import cardAxel from "../components/card/cardAxel.vue";
+import cardMeldryck from "../components/card/cardMeldryck.vue";
+
+export default {
+
+  components: {
+    cardIlkay,
+    cardAxel,
+    cardMeldryck,
+  },
+};
+
+</script>

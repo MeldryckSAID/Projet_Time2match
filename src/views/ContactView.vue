2a454d0d434b49f3902518da407461a7970a307a
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-05 16:23:37 +0200

    base fonctionnel

---
+++ src/views/ContactView.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <MenuView />
+  </header>
+  <main class="min-h-screen bg-Noirr"></main>
+
+  <footer>
+    <FooterView></FooterView>
+  </footer>
+</template>
+
+<script>
+import MenuView from "../components/MenuView.vue";
+import FooterView from "../components/FooterView.vue";
+
+export default {
+  components: {
+    MenuView,
+    FooterView,
+  },
+};
+</script>

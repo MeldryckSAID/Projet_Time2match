9087c49658cba4ca47fd48e27db7fd4602d2894f
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-05 21:32:23 +0200

    page sport

--- src/views/SportsView.vue
+++ src/views/SportsView.vue
@@ -1,49 +1,86 @@
 <template>
-<header>
-  <MenuView></MenuView>
-</header>
-<main>
-<div class=" bg-Noirr text-Blancc ">
-  <div>
-    <h2 class="text-center font-quicksand font-semibold text-8xl p-8">Les sports sur time2match</h2>
-    <p>Non, vous ne trouverez pas le kayak.</p>
-  </div>
+  <header>
+    <MenuView></MenuView>
+  </header>
+  <main class="bg-Noirr text-Blancc">
+    <div class="justify-center">
+      <div>
+        <h2 class="py-15 p-8 text-center font-quicksand text-8xl font-semibold">
+          Les sports sur Time<span class="text-Orangee">2</span>match
+        </h2>
+        <p class="py-5">Non, vous ne trouverez pas le kayak.</p>
+      </div>
 
-  <div>
-    <div class="columns-3">
-      <IconFootball class="h-1/5 w-1/5" />
-      <h3 class="text-3xl uppercase">le football</h3>
-      <IconFootball class="h-1/5 w-1/5" />
+      <div class="py-5">
+        <div class="flex columns-3 items-center justify-center">
+          <div class="flex items-center justify-around py-5">
+            <IconFootball class="mx-4 h-1/5 w-1/5" />
+            <h3 class="text-3xl uppercase">Le football</h3>
+            <IconFootball class="mx-4 h-1/5 w-1/5" />
+          </div>
+        </div>
+        <p class="py-4 text-center">
+          Icônique sport du ballon rond, le football est le
+          <br />
+          sport le plus pratiqué en FRANCE, on compte <br />
+          notamment plus de 2 198 703 licenciés.
+        </p>
+
+        <RouterLink class="flex justify-center" to="/terrains">
+          <span class="text-Orangee underline">Trouver un espace de football</span>
+        </RouterLink>
+      </div>
+
+      <div class="py-5">
+        <div class="flex columns-3 items-center justify-center">
+          <div class="flex items-center justify-around py-5">
+            <IconBasketball class="mx-4 h-1/5 w-1/5" />
+            <h3 class="text-3xl uppercase">le basketball</h3>
+            <IconBasketball class="mx-4 h-1/5 w-1/5" />
+          </div>
+        </div>
+        <p class="text-center">
+          Le basketball est le 4ème sport le plus pratiqué en <br />
+          FRANCE, on compte notamment <br />
+          683 000 licenciés.
+        </p>
+
+        <RouterLink class="flex justify-center" to="/terrains">
+          <span class="text-Orangee underline">Trouver un espace de basketball</span>
+        </RouterLink>
+      </div>
+
+      <div class="py-5">
+        <div class="flex columns-3 items-center justify-center">
+          <div class="flex items-center justify-around py-5">
+            <IconVolleyball class="mx-4 h-1/5 w-1/5" />
+            <h3 class="text-3xl uppercase">le volleyball</h3>
+            <IconVolleyball class="mx-4 h-1/5 w-1/5" />
+          </div>
+        </div>
+        <p class="text-center">
+          Plus souvent pratiqué en loisir sur les côtes, <br />
+          le volleyball compte néanmoins <br />
+          111 378 licenciés.
+        </p>
+        <RouterLink class="flex justify-center" to="/terrains">
+          <span class="text-Orangee underline">Trouver un espace de volleyball</span>
+        </RouterLink>
+      </div>
+      
     </div>
-    <p>
-      Icônique sport du ballon rond, le football est le sport le plus pratiqué en FRANCE, on compte notamment plus de 2 198 703 licenciés.
-    </p>
-    <span class="text-Orangee underline">Trouver un espace de football</span>
-  </div>
+<div class="flex justify-center py-5">
+<div class="border-4 w-fit m-5  flex flex-col justify-center items-center rounded-xl border-Orangee py-3">
+        <p class="font-quicksand font-light text-lg"> Vous ne trouvez pas votre sport favori ? Proposez-le nous ! </p>
+        <h5 class="font-quicksand text-2xl font-light text-Orangee">Nous contacter</h5>
+      </div>
+</div>
+    
 
-  <div>
-    <div class="columns-3">
-      <IconBasketball class="h-1/5 w-1/5" />
-      <h3 class="text-3xl uppercase">le basketball</h3>
-      <IconBasketball class="h-1/5 w-1/5" />
-    </div>
-    <p>Le basketball est le 4ème sport le plus pratiqué en FRANCE, on compte notamment 683 000 licenciés.</p>
-    <span class="text-Orangee underline">Trouver un espace de basketball</span>
-  </div>
-
-  <div>
-    <div class="columns-3">
-      <IconVolleyball class="h-1/5 w-1/5" />
-      <h3 class="text-3xl uppercase">le volleyball</h3>
-      <IconVolleyball class="h-1/5 w-1/5" />
-    </div>
-    <p>Plus souvent pratiqué en loisir sur les côtes, le volleyball compte néanmoins 111 378 licenciés.</p>
-    <span class="text-Orangee underline">Trouver un espace de volleyball</span>
-  </div>
-
-  <p>Vous ne trouvez pas votre sport favori ? Proposez-le nous ! Nous contacter</p>
-  </div>
   </main>
+  <footer>
+    <FooterView />
+  </footer>
 </template>
 
 <script>
@@ -51,6 +88,7 @@ import IconFootball from "../components/icones-sports.vue/icon-football.vue";
 import IconBasketball from "../components/icones-sports.vue/icon-basketball.vue";
 import IconVolleyball from "../components/icones-sports.vue/icon-volleyball.vue";
 import MenuView from "../components/MenuView.vue";
+import FooterView from "../components/FooterView.vue";
 
 export default {
   name: "SportsView",
@@ -58,8 +96,9 @@ export default {
     IconFootball,
     IconBasketball,
     IconVolleyball,
-    MenuView
-},
+    MenuView,
+    FooterView,
+  },
   data() {
     return {};
   },

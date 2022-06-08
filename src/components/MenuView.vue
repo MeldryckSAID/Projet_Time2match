64a8af14d0df30d21cba6c2c9e7f9b047bff78ca
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-08 10:57:06 +0200

    ilkay in the game

--- src/components/MenuView.vue
+++ src/components/MenuView.vue
@@ -27,8 +27,7 @@
       </RouterLink>
     </div>
 
-      <div class="flex flex-col  hover:border-b-4 hover:border-b-white">
-        
+    <div class="flex flex-col hover:border-b-4 hover:border-b-white">
       <RouterLink class="block py-2 px-4" to="/partenaires">
         <Logopartenaire></Logopartenaire>
         <h5 class="font-quicksand text-base font-bold uppercase text-Noirr">Partenaires</h5>
@@ -36,9 +35,27 @@
     </div>
 
     <div class="flex flex-col justify-center hover:border-b-4 hover:border-b-white">
-      <RouterLink class="block py-2 px-4" to="/partenaires">
-        <LogoSeco class="text-Noirr"></LogoSeco>
+      <RouterLink class="block py-2 px-4" to="/">
+        <div class="grid">
+          <span v-if="avatar != null" class="mr-3">
+            <img class="avatar h-10 w-auto" :src="avatar" />
+            <RouterLink to="/" class="text-xs">{{ name }}</RouterLink>
+          </span>
+        </div>
+        <div v-if="avatar != null" class="hidden pt-9">
+          <LogoSeco class="text-Noirr"></LogoSeco>
+        </div>
+        <div v-else class="pt-9">
+          <LogoSeco class="text-Noirr"></LogoSeco>
+        </div>
       </RouterLink>
+    </div>
+
+    <div class="menu" v-if="isAdmin">
+      <select>
+        <option>TERRAINS</option>
+        <option>PARTIES</option>
+      </select>
     </div>
 
     <RouterView />
@@ -53,7 +70,130 @@ import LogoSports from "./menu/logosports.vue";
 import LogoSeco from "./menu/logoseco.vue";
 import Logopartenaire from "./menu/logopartenaire.vue";
 
+// Fonctions Firestore
+import { getFirestore, collection, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+
+// Fonctions Storage
+import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
+
+// Fonction authentification
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+
+// Import emetteur de main.js
+import { emitter } from "../main";
+
 export default {
-  components: { Logo, LogoTerrain, LogoPartie, LogoSeco, LogoSports, Logopartenaire },
+  name: "App",
+  components: {
+    Logo,
+    LogoTerrain,
+    LogoPartie,
+    LogoSeco,
+    LogoSports,
+    Logopartenaire,
+  },
+  data() {
+    return {
+      MenuOpen: false,
+      user: {
+        // User connecté
+        email: null,
+        password: null,
+      },
+      userInfo: null, // Informations complémentaires user connecté
+      name: "Vidéo", // Titre de l'application ou nom du user
+      avatar: null, // Avatar / image du user connecté
+      isAdmin: false, // Si l'utilisateur est ou non administrateur
+    };
+  },
+  mounted() {
+    // Vérifier si un user connecté existe déjà
+    // Au lancement de l'application
+    this.getUserConnect();
+
+    // Ecoute de l'évènement de connexion
+    emitter.on("connectUser", (e) => {
+      // Récupération du user
+      this.user = e.user;
+      console.log("App => Reception user connecté", this.user);
+      // Recherche infos complémentaires du user
+      this.getUserInfo(this.user);
+    });
+
+    // Ecoute de l'évènement de deconnexion
+    emitter.on("deConnectUser", (e) => {
+      // Récupération user
+      this.user = e.user;
+      console.log("App => Reception user deconnecté", this.user);
+      // Réinitialisation infos complémentaires user
+      this.userInfo = null;
+      this.name = "Vidéo";
+      this.avatar = null;
+      this.isAdmin = false;
+    });
+  },
+  methods: {
+    // Obtenir les informations du user connecté
+    async getUserConnect() {
+      await getAuth().onAuthStateChanged(
+        function (user) {
+          if (user) {
+            // Récupération du user connecté
+            this.user = user;
+            // Recherche de ses infos complémentaires
+            this.getUserInfo(this.user);
+          }
+        }.bind(this)
+      );
+      // Noter le bind(this), cas des zones isolées
+      // lors de 2 strucutres imbiquées, Vue perd la visibilité
+      // des données
+      // On les ré injecte par le mot-clef this
+    },
+
+    async getUserInfo(user) {
+      // Rechercher les informations complémentaires de l'utilisateur
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document participant
+      const dbUsers = collection(firestore, "users");
+      // Recherche du user par son uid
+      const q = query(dbUsers, where("uid", "==", user.uid));
+      await onSnapshot(q, (snapshot) => {
+        this.userInfo = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log("userInfo", this.userInfo);
+        // userInfo étant un tableau, onn récupère
+        // ses informations dans la 1° cellule du tableau : 0
+        this.name = this.userInfo[0].login;
+        this.isAdmin = this.userInfo[0].admin;
+        // Recherche de l'image du user sur le Storage
+        const storage = getStorage();
+        // Référence du fichier avec son nom
+        const spaceRef = ref(storage, "users/" + this.userInfo[0].avatar);
+        getDownloadURL(spaceRef)
+          .then((url) => {
+            this.avatar = url;
+          })
+          .catch((error) => {
+            console.log("erreur downloadUrl", error);
+          });
+      });
+    },
+  },
 };
 </script>
+
+<style>
+/* Import Styles application */
+@import "../assets/style.css";
+
+.avatar {
+  vertical-align: middle;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+// Fonction d'authentification
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+
+// Fonctions Firestore
+import { getFirestore, collection, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+
 import AccueilView from "../views/AccueilView.vue";
 import TerrainsView from "../views/TerrainsView.vue";
 import PartiesView from "../views/PartiesView.vue";
@@ -8,13 +15,14 @@ import AProposView from "../views/AProposView.vue";
 import MentionLegalesView from "../views/MentionLegalesView.vue";
 import PageCoView from "../views/PageCoView.vue";
 import ContactView from "../views/ContactView.vue";
-
+import moncompteView from "../views/moncompteView.vue";
+import ChatView from "../views/ChatView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "AccueilView", component: AccueilView },
-    { path: "/terrains", name: "TerrainsView", component: TerrainsView },
+    { path: "/terrains", name: "TerrainsView", component: TerrainsView, beforeEnter: guard },
     { path: "/parties", name: "PartiesView", component: PartiesView },
     { path: "/sports", name: "SportsView", component: SportsView },
     { path: "/partenaires", name: "PartenairesView", component: PartenairesView },
@@ -22,8 +30,51 @@ const router = createRouter({
     { path: "/mentionlegales", name: "MentionsLegalesView", component: MentionLegalesView },
     { path: "/pageco", name: "PageCoView", component: PageCoView },
     { path: "/contact", name: "ContactView", component: ContactView },
-
+    { path: "/moncompte", name: "moncompteView", component: moncompteView },
+    { path: "/chat", name: "ChatView", component: ChatView },
   ],
 });
+
+// On créé un guard : Observateur (fonction) permettant de savoir si un utilisateur
+// a le droit d'utiliser une route
+// paramètres : to : d'où il vient, from où il veut aller, next où il doit aller après contrôle
+function guard(to, from, next) {
+  // recherche utilisateur connecté
+  getAuth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User connecté
+      console.log("router OK => user ", user);
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document participant
+      const dbUsers = collection(firestore, "users");
+      // Recherche du user par son uid
+      const q = query(dbUsers, where("uid", "==", user.uid));
+      onSnapshot(q, (snapshot) => {
+        let userInfo = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // userInfo étant un tableau, on récupère
+        // ses informations dans la 1° cellule du tableau : 0
+        let isAdmin = userInfo[0].admin;
+        if (isAdmin) {
+          // Utilisateur administrateur, on autorise la page/vue
+          next(to.params.name);
+          return;
+        } else {
+          // Utilisateur non administrateur, renvoi sur accueil
+          alert("Vous n'avez pas l'autorisation pour cette fonction");
+          next({ name: "accueilView" });
+          return;
+        }
+      });
+    } else {
+      // Utilisateur non connecté, renvoi sur accueil
+      console.log("router NOK => user ", user);
+      next({ name: "accueilView" });
+    }
+  });
+}
 
 export default router;
